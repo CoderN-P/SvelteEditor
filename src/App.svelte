6fe7615558075e93d61a47fdc5789be9911e2776
CoderN-P <neel.parpia@gmail.com>
2024-06-20 10:53:57 -0700

    Finished demo page

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,9 @@
 <script lang="ts">
   import 'highlight.js/styles/tokyo-night-dark.css';
   import Editor from './Editor.svelte';
-  import { onMount, onDestroy } from 'svelte';
 
-  let resizeObserver: ResizeObserver | null = null;
-
-  onMount(() => {
-      const gradient = document.getElementById('gradient');
-      const editor = document.getElementById('mainEditor');
-
-      if (!editor || !gradient) {
-          return;
-      }
-
-      function outputsize() {
-          if (editor && gradient) {
-              gradient.style.height = `${editor.clientHeight}px`;
-          }
-      }
-
-      outputsize();
-
-    resizeObserver = new ResizeObserver(outputsize);
-    resizeObserver.observe(editor);
-
-  });
-
-  onDestroy(() => {
-      if (resizeObserver) {
-          resizeObserver.disconnect();
-      }
-  });
 
   let code = "npm install editor-for-svelte";
-
 </script>
 
 <style>
@@ -62,9 +32,10 @@
         </div>
         <h1 class="font-bold bg-clip-text mt-[20vh] text-6xl text-gray-900 text-transparent bg-gradient-to-r from-orange-500 to-purple-500">Svelte Editor</h1>
         <p class="text-gray-600 mt-2 dark:text-gray-400">A <strong>simple</strong>, but <strong>customizable</strong> code editor for svelte</p>
-        <div class="relative mt-[5vh] w-3/4">
-            <Editor id="mainEditor" lines langSelector lineSelector bind:value={code} lineSelectorClass="bg-gray-900/50 hover:bg-gray-900/60" dots langSelectorClass="bg-gray-900/50 hover:bg-gray-900/60" language="shell" maxHeight="40vh" lineNumberClass="!border-0 bg-gray-900/30 rounded-md" defaultTextClass="text-gray-200/50 text-sm ml-4" caretColor="rgba(255, 255, 255, 0.5)" class="absolute w-full !ml-0 !top-0 !left-0 text-gray-200/50 bg-gray-950 backdrop-blur z-50 !m-8"/>
-            <div id="gradient" class="mt-8 h-10 absolute bg-gradient-to-r from-orange-500 to-purple-500 rounded-lg blur-sm w-full z-0"></div>
+        <div class="relative flex mt-[5vh] w-3/4">
+            <Editor id="mainEditor" minHeight="40vh" lines langSelector lineSelector bind:value={code} lineSelectorClass="bg-gray-900/50 hover:bg-gray-900/60" dots langSelectorClass="bg-gray-900/50 hover:bg-gray-900/60" language="shell" maxHeight="40vh" lineNumberClass="!border-0 bg-gray-900/30 rounded-md" defaultTextClass="text-gray-200/50 text-sm ml-4" caretColor="rgba(255, 255, 255, 0.5)" class="absolute w-full !ml-0 !top-0 !left-0 text-gray-200/50 bg-gray-950 backdrop-blur z-50 !m-8">
+            </Editor>
+            <div id="gradient" style="height:40vh;" class="mt-8 h-full absolute bg-gradient-to-r from-orange-500 to-purple-500 rounded-lg blur-sm w-full z-0"></div>
         </div>
     </div>
 </main>
